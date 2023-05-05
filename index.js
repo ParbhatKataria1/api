b11b30d353499d6638f51952643f2f57ea8f54d3
ParbhatKataria1 <varshagagangandhi@gmail.com>
2023-05-06 00:41:11 +0530

    done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { connection } = require("./db");
 const { user } = require("./Routes/user.routes");
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 
